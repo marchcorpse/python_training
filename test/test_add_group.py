b72de594bdfb4d4f11c4c088aa5769f72cd2b1e7
marchcorpse <37371204+marchcorpse@users.noreply.github.com>
2018-05-21 16:20:41 +0700

    Выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="sdsd", header="sds", footer="sdsda"))
+    app.group.create_group(Group(name="sdsd", header="sds", footer="sdsda"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create_group(Group(name="", header="", footer=""))
     app.session.logout()
 
