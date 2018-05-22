41dc73f5f67d6c9b0823eb8bd3b77ad4bbe97e2b
marchcorpse <37371204+marchcorpse@users.noreply.github.com>
2018-05-22 18:12:46 +0700

    Login и Logout вынесены при создании фикстуры

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="first", middlename="middle", lastname="last", address="address"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create_contact(Contact(firstname="", middlename="", lastname="", address=""))
-    app.session.logout()
