3d474315d2c1c8e6543630dfb4b918dc21ec7ee2
marchcorpse <37371204+marchcorpse@users.noreply.github.com>
2018-05-22 11:24:56 +0700

    Добален тест изменяющий первую группу

---
+++ test/test_edit_group.py
@@ -0,0 +1,7 @@
+from model.group import Group
+
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="1", header="2", footer="3"))
+    app.session.logout()
