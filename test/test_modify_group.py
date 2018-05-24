00a9b580da3bb25734803546ed181b0b13dbb86c
marchcorpse <37371204+marchcorpse@users.noreply.github.com>
2018-05-24 17:55:00 +0700

    upd for last commit

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -13,12 +13,12 @@ def test_modify_group_name(app):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-    old_groups = app.group.get_group_list()
-    group = Group(header="New header")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+# def test_modify_group_header(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(header="New header")
+#     group.id = old_groups[0].id
+#     app.group.modify_first_group(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) == len(new_groups)
+#     old_groups[0] = group
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
