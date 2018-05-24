063f69b2613648f06c968303bed43332c16c58cb
marchcorpse <37371204+marchcorpse@users.noreply.github.com>
2018-05-24 16:33:35 +0700

    Для тестов добавления и модификации групп добавлено сравнение и сортировка групп до и после прохода методов

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,19 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create_group(Group(name="sdsd", header="sds", footer="sdsda"))
+    group = Group(name="sdsd", header="sds", footer="sdsda")
+    app.group.create_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    app.group.create_group(Group(name="", header="", footer=""))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+# def test_add_empty_group(app):
+#     old_groups = app.group.get_group_list()
+#     group = Group(name="", header="", footer="")
+#     app.group.create_group(group)
+#     new_groups = app.group.get_group_list()
+#     assert len(old_groups) + 1 == len(new_groups)
+#     old_groups.append(group)
+#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
