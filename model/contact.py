b7a962dbe4aeefd6e9292c1a4b8d669650268c26
marchcorpse <37371204+marchcorpse@users.noreply.github.com>
2018-05-25 13:24:21 +0700

    В тесте удаления контактов добавлено сравнение списков контактов по содержанию до прохода метода и после.

--- model/contact.py
+++ model/contact.py
@@ -6,3 +6,9 @@ class Contact:
         self.lastname = lastname
         self.address = address
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.firstname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname
